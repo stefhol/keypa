41ba10b63e7db14f7d6320cb21de15fec8b21e64
stefhol <hoef.stef@pm.me>
2022-12-12 23:14:05 +0100

    update model

--- migrations/20221020190412_setup.down.sql
+++ migrations/20221020190412_setup.down.sql
@@ -1,14 +1,19 @@
-drop view view_active_doors_by_user;
-drop view view_active_keycards;
+drop view if exists view_active_doors_by_user;
+-- drop view view_active_keycards;
 
-alter table tbl_keycard drop constraint if EXISTS fk_request_keycard;
-drop table if exists tbl_door_access_history;
+alter table if exists tbl_keycard drop constraint if EXISTS fk_request_keycard;
+alter table if exists tbl_request_entrance drop constraint if EXISTS fk_request_request_entrance;
+alter table if exists tbl_request_department drop constraint if EXISTS fk_request_request_department;
+
 drop table if exists tbl_change_rights_history;
 drop table if exists tbl_request_history;
 drop table if exists tbl_door_to_request_history;
+drop table if exists tbl_door_to_request;
 drop table if exists tbl_request_comment;
 drop table if exists tbl_temp_keycard_request;
 drop table if exists tbl_door_request;
+drop table if exists tbl_request_department;
+drop table if exists tbl_request_entrance;
 
 -- proposal
 drop table if exists tbl_proposal_entrance;
@@ -16,9 +21,7 @@ drop table if exists tbl_proposal_department;
 drop table if exists tbl_proposal;
 
 --request
-drop table if exists tbl_door_to_request;
-drop table if exists tbl_keycard_request;
-drop table if exists tbl_request_base;
+drop table if exists tbl_request;
 drop table if exists tbl_keycard_history;
 
 drop table if exists tbl_keycard;
@@ -38,7 +41,7 @@ drop table if exists tbl_building;
 drop table if exists tbl_user;
 drop table if exists tbl_role;
 drop table if exists tbl_status;
-DROP TYPE history_action;
-DROP TYPE history_role;
-DROP TYPE history_request_type;
+DROP TYPE if exists history_action;
+DROP TYPE if exists history_role;
+DROP TYPE if exists history_request_type;
 drop table if exists _sqlx_migrations;
--- migrations/20221020190412_setup.up.sql
+++ migrations/20221020190412_setup.up.sql
@@ -91,8 +91,8 @@ create table if not exists tbl_admin
 create table if not exists tbl_change_rights_history
 (
     change_rights_history_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    action                   history_action                      not null,
-    internal_role            history_role               not null,
+    action                   history_action              not null,
+    internal_role            history_role                not null,
     target_user_id           uuid                        not null,
     changed_by               uuid                        not null,
     changed_at               timestamp without time zone not null default timezone('utc', now()),
@@ -103,40 +103,34 @@ create table if not exists tbl_change_rights_history
 
 create table if not exists tbl_keycard
 (
-    keycard_id uuid primary key DEFAULT uuid_generate_v4(),
-    request_id uuid not null
+    keycard_id     uuid primary key DEFAULT uuid_generate_v4(),
+    is_lost        bool    not null default false,
+    is_locked      boolean not null default false,
+    is_deactivated boolean not null default false,
+    is_given_back boolean not null default false,
+    request_id     uuid    not null
 );
-create table if not exists tbl_proposal
+
+create table if not exists tbl_request_entrance
 (
-    proposal_id  uuid primary key                     default uuid_generate_v4(),
-    requester_id uuid                        not null,
-    created_at   timestamp without time zone not null default timezone('utc', now()),
-    changed_at   timestamp without time zone not null default timezone('utc', now()),
-    description  text,
-    active_until timestamp without time zone,
-    is_finished  bool                        not null default false,
-    foreign key (requester_id) references tbl_user (user_id)
-);
-create table if not exists tbl_proposal_entrance
-(
-    proposal_entrance_id uuid primary key default uuid_generate_v4(),
-    proposal_id          uuid not null,
-    foreign key (proposal_id) references tbl_proposal (proposal_id),
-    building_id          uuid not null,
+    request_entrance_id uuid primary key default uuid_generate_v4(),
+    request_id          uuid not null,
+
+    building_id         uuid not null,
     foreign key (building_id) references tbl_building (building_id),
-    proposed_rooms       text not null
+    proposed_rooms      text not null
 );
-create table if not exists tbl_proposal_department
+create table if not exists tbl_request_department
 (
-    proposal_id   uuid not null,
+    request_id    uuid not null,
     department_id uuid not null,
-    primary key (proposal_id, department_id),
-    foreign key (proposal_id) references tbl_proposal (proposal_id),
+    primary key (request_id, department_id),
+
     foreign key (department_id) references tbl_department (department_id)
 
 );
 
-create table if not exists tbl_request_base
+create table if not exists tbl_request
 (
     request_id   uuid primary key                     DEFAULT uuid_generate_v4(),
     requester_id uuid                        not null,
@@ -145,67 +139,67 @@ create table if not exists tbl_request_base
     active_until timestamp without time zone,
     description  text,
     status_id    uuid,
+    is_proposal   boolean                     not null default false,
     active       boolean                     not null default true,
     accept       boolean                     not null default false,
     reject       boolean                     not null default false,
     pending      boolean                     not null default true,
     foreign key (requester_id) references tbl_user (user_id),
-    foreign key (status_id) references tbl_status (status_id)
+    foreign key (status_id) references tbl_status (status_id),
+    keycard_id   uuid,
+    foreign key (keycard_id) references tbl_keycard (keycard_id)
 );
-
-create table if not exists tbl_keycard_request
+create table if not exists tbl_request_history
 (
-    request_id uuid primary key,
-    keycard_id uuid,
-    is_lost    bool not null default false,
-    foreign key (keycard_id) references tbl_keycard (keycard_id),
-    foreign key (request_id) references tbl_request_base (request_id)
-);
-create table if not exists tbl_request_history(
     request_history_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    changed_by uuid not null,
-    request_id uuid not null,
-    action history_action not null,
-    request_type history_request_type not null,
-    requester_id uuid                        not null,
-    created_at   timestamp without time zone not null,
-    changed_at   timestamp without time zone not null,
-    active_until timestamp without time zone,
-    description  text,
-    status_id    uuid,
-    active       boolean not null,
-    accept       boolean not null,
-    reject       boolean not null,
-    pending      boolean not null,
-    keycard_id    uuid,
-    is_payed      boolean,
-    is_given_back boolean,
-    is_lost    bool not null default false,
+    changed_by         uuid                        not null,
+    request_id         uuid                        not null,
+    action             history_action              not null,
+    request_type       history_request_type        not null,
+    requester_id       uuid                        not null,
+    created_at         timestamp without time zone not null,
+    changed_at         timestamp without time zone not null,
+    active_until       timestamp without time zone,
+    description        text,
+    status_id          uuid,
+    active             boolean                     not null,
+    accept             boolean                     not null,
+    reject             boolean                     not null,
+    pending            boolean                     not null,
+    keycard_id         uuid,
+    is_lost            bool                        not null default false,
+    is_payed      bool not null DEFAULT false,
+    is_given_back bool not null DEFAULT false,
     foreign key (keycard_id) references tbl_keycard (keycard_id),
     foreign key (requester_id) references tbl_user (user_id),
     foreign key (changed_by) references tbl_user (user_id),
-    foreign key (request_id) references tbl_request_base (request_id),
+    foreign key (request_id) references tbl_request (request_id),
     foreign key (status_id) references tbl_status (status_id)
 );
-create table if not exists tbl_door_to_request_history(
+create table if not exists tbl_door_to_request_history
+(
     door_to_request_history_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    door_id    uuid not null,
-    request_id uuid not null,
-    action history_action not null,
-    changed_by uuid not null,
+    door_id                    uuid           not null,
+    request_id                 uuid           not null,
+    action                     history_action not null,
+    changed_by                 uuid           not null,
     foreign key (door_id) references tbl_door (door_id),
-    foreign key (request_id) references tbl_request_base (request_id),
+    foreign key (request_id) references tbl_request (request_id),
     foreign key (changed_by) references tbl_user (user_id)
 );
 alter table tbl_keycard
-    ADD constraint fk_request_keycard foreign key (request_id) references tbl_request_base (request_id);
+    ADD constraint fk_request_keycard foreign key (request_id) references tbl_request (request_id);
+alter table tbl_request_entrance
+    ADD constraint fk_request_request_entrance foreign key (request_id) references tbl_request (request_id);
+alter table tbl_request_department
+    ADD constraint fk_request_request_department foreign key (request_id) references tbl_request (request_id);
 create table if not exists tbl_keycard_history
 (
     keycard_history_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    keycard_id uuid                        not null,
-    door_id    uuid                        not null,
-    used_at    timestamp without time zone not null default timezone('utc', now()),
-    success    boolean not null,
+    keycard_id         uuid                        not null,
+    door_id            uuid                        not null,
+    used_at            timestamp without time zone not null default timezone('utc', now()),
+    success            boolean                     not null,
     foreign key (keycard_id) references tbl_keycard (keycard_id),
     foreign key (door_id) references tbl_door (door_id)
 );
@@ -215,14 +209,14 @@ create table if not exists tbl_door_to_request
     door_id    uuid,
     request_id uuid,
     foreign key (door_id) references tbl_door (door_id),
-    foreign key (request_id) references tbl_request_base (request_id),
+    foreign key (request_id) references tbl_request (request_id),
     primary key (door_id, request_id)
 );
 
 create table if not exists tbl_door_request
 (
     request_id uuid primary key,
-    foreign key (request_id) references tbl_request_base (request_id)
+    foreign key (request_id) references tbl_request (request_id)
 );
 create table if not exists tbl_request_comment
 (
@@ -231,44 +225,15 @@ create table if not exists tbl_request_comment
     user_id    uuid                        not null,
     comment    text                        not null,
     written_at timestamp without time zone not null default timezone('utc', now()),
-    foreign key (request_id) references tbl_request_base (request_id),
+    foreign key (request_id) references tbl_request (request_id),
     foreign key (user_id) references tbl_user (user_id)
 );
 
-create table if not exists tbl_temp_keycard_request
-(
-    request_id    uuid primary key,
-    keycard_id    uuid,
-    is_payed      bool not null DEFAULT false,
-    is_given_back bool not null DEFAULT false,
-    foreign key (keycard_id) references tbl_keycard (keycard_id),
-    foreign key (request_id) references tbl_request_base (request_id)
-);
-
--- History of Door access
-create table if not exists tbl_door_access_history
-(
-    door_history_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    door_id         uuid not null,
-    user_id         uuid not null,
-    foreign key (door_id) references tbl_door (door_id),
-    foreign key (user_id) references tbl_user (user_id),
-    deactivated_at  timestamp without time zone,
-    activated_at    timestamp without time zone default timezone('utc', now())
-);
-
-create view view_active_keycards as
-select tbl_request_base.active_until, tbl_keycard_request.keycard_id
-from tbl_request_base
-         join tbl_keycard_request on tbl_keycard_request.request_id = tbl_request_base.request_id
-where tbl_keycard_request.is_lost = false;
-
-
 
 create view view_active_doors_by_user as
-select tbl_request_base.requester_id as user_id, tbl_door_to_request.door_id, tbl_request_base.active_until
-from tbl_request_base
-join tbl_door_request on tbl_request_base.request_id = tbl_door_request.request_id
-join tbl_door_to_request on tbl_request_base.request_id = tbl_door_to_request.request_id
-where tbl_request_base.active = true;
+select tbl_request.requester_id as user_id, tbl_door_to_request.door_id, tbl_request.active_until
+from tbl_request
+         join tbl_door_request on tbl_request.request_id = tbl_door_request.request_id
+         join tbl_door_to_request on tbl_request.request_id = tbl_door_to_request.request_id
+where tbl_request.active = true;
 -- Nutzer Id, Tür Id und Ablaufzeit des Zugriffs von Nutzer auf die Tür
